beea6bd6316d8923c5191d5c560c266870de0a13
zaCKoZAck0 <69889382+zaCKoZAck0@users.noreply.github.com>
2023-03-17 11:44:45 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+require('dotenv');
+const port = 3010;
+const path = require('path');
+const { Configuration, OpenAIApi } = require('openai');
+const configuration = new Configuration({
+  organization: 'org-SsEvIFMhp846iNdd79sfbU3l',
+  apiKey: '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',
+});
+const openai = new OpenAIApi(configuration);
+
+const FRONTEND_URL = '';
+
+app.options('*', cors({ origin: FRONTEND_URL, optionsSuccessStatus: 200 }));
+
+app.use(cors({ origin: FRONTEND_URL, optionsSuccessStatus: 200 }));
+app.use(bodyParser.json());
+
+app.use(express.static('static'));
+
+app.get('/chat', async (req, res) => {
+  const query = req.query.query;
+  const completion = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    messages: [
+      {
+        role: 'system',
+        content:
+          'You are a useful shopping assitant that recommends products according to user query. You just return name and price for the query in form list seperated if list of products is asked, or return bullet points just try to make the answer concise, if user asks you to tell you more about the product return description then only. You show currency as INR and show personalised results for India',
+      },
+      {
+        role: 'user',
+        content: query,
+      },
+    ],
+  });
+  res.json({ message: completion.data.choices[0].message.content });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
